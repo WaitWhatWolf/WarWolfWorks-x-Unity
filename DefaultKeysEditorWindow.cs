0a3127fb9dbeb59e059200847d0ca47d2fa4e513
Mariusz <58935825+WhiteWarriorWolf@users.noreply.github.com>
2020-01-04 17:56:35 +0100

    Initial Commit

---
+++ DefaultKeysEditorWindow.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using WarWolfWorks.Utility;
+
+namespace WarWolfWorks.EditorBase
+{
+    public class KeyCodeEditorWindow : EditorWindow
+    {
+        public const string WindowName = "Default Keys Customizer";
+
+        private Vector3 ScrollPosition;
+
+        private bool pop;
+
+        private bool pop2;
+
+        private bool pop3;
+
+        private bool pop4;
+
+        private bool pop5;
+
+        private string keySave;
+
+        private string keycodeSave;
+
+        private (string, KeyCode)[] Keys;
+
+        [MenuItem("WWWLibrary/Default Keys Customizer")]
+        public static void ShowWindow()
+        {
+            KeyCodeEditorWindow window = EditorWindow.GetWindow<KeyCodeEditorWindow>("Default Keys Customizer");
+            window.Show();
+            window.minSize = new Vector2(250f, 350f);
+            window.maxSize = new Vector2(350f, 900f);
+            window.position = new Rect(1200f, 300f, 350f, 500f);
+        }
+
+        private void OnEnable()
+        {
+            UpdateKeys();
+        }
+
+        private void UpdateKeys()
+        {
+            try { Keys = DefaultKeys.GetAllKeys(); }
+            catch { goto DefaultSetter; }
+            if (Keys != null)
+                return;
+
+            DefaultSetter:
+            Keys = new (string, KeyCode)[]
+            {
+                ("ConsoleKey", KeyCode.F1)
+            };
+        }
+
+        private void OnGUI()
+        {
+            if (!DefaultKeys.IsOptimized)
+            {
+                EditorGUILayout.BeginVertical();
+                ScrollPosition = EditorGUILayout.BeginScrollView(ScrollPosition);
+
+                for (int num = Keys.Length - 1; num >= 0; num--)
+                {
+                    EditorGUILayout.LabelField($"{Keys[num].Item1}: {Keys[num].Item2}");
+                }
+
+                EditorGUILayout.EndScrollView();
+                EditorGUILayout.EndVertical();
+
+                pop = GUILayout.Button("Save Keys");
+                if (pop && Keys != null)
+                {
+                    for (int i = 0; i < Keys.Length; i++)
+                    {
+                        DefaultKeys.ChangeKey(Keys[i]);
+                    }
+                }
+                pop2 = GUILayout.Button("Update Keys");
+                if (pop2)
+                {
+                    UpdateKeys();
+                }
+                keySave = EditorGUILayout.TextField("Custom Key", keySave, Array.Empty<GUILayoutOption>());
+                keycodeSave = EditorGUILayout.TextField("Parsed Value", keycodeSave, Array.Empty<GUILayoutOption>());
+                KeyCode item = KeyCode.None;
+                if (keycodeSave != string.Empty)
+                {
+                    try
+                    {
+                        item = Hooks.Parse<KeyCode>(keycodeSave);
+                    }
+                    catch
+                    {
+                        EditorGUILayout.HelpBox("The parsed value given is invalid! Make sure there are no spaces in your input.", MessageType.Warning);
+                    }
+                }
+                pop3 = GUILayout.Button("Save Custom Key");
+                if (pop3)
+                {
+                    DefaultKeys.AddKey((keySave, item));
+                    UpdateKeys();
+                }
+                pop4 = GUILayout.Button("Remove Custom Key");
+                if (pop4)
+                {
+                    DefaultKeys.RemoveKey(keySave);
+                    UpdateKeys();
+                }
+            }
+            else
+            {
+                EditorGUILayout.LabelField("Cannot modify keys while Optimized mode is active!", Array.Empty<GUILayoutOption>());
+                if (!Application.isPlaying)
+                {
+                    pop5 = GUILayout.Button("Force Disable Optimization");
+                    if (pop5)
+                    {
+                        DefaultKeys.Unoptimize();
+                    }
+                }
+            }
+            for (int j = 0; j < 4; j++)
+            {
+                EditorGUILayout.Space();
+            }
+            EditorGUILayout.LabelField("Current Optimization State: " + (DefaultKeys.IsOptimized ? "Active" : "Inactive"));
+        }
+    }
+}
---
+++ EnumListDisplayer.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using WarWolfWorks.Internal;
+using WarWolfWorks.Utility;
+
+namespace WarWolfWorks.EditorBase
+{
+    public class EnumListDisplayer : EditorWindow
+    {
+        [MenuItem("WWWLibrary/Enum Displayer")]
+        public static void Enable()
+        {
+            GetWindow<EnumListDisplayer>("Enum List Displayer").Show();
+        }
+
+        private Type CurrentType = null;
+        private string ParsedType = "";
+
+        bool flag;
+
+        private bool ActivatedWindow;
+
+        public (string[] EnumName, int[] EnumVal) EnumValues;
+        public (string[] EnumName, short[] EnumVal) EnumValuesShort;
+        public (string[] EnumName, long[] EnumVal) EnumValuesLong;
+
+        private Vector3 ScrollPosition;
+
+        enum IntType : short
+        {
+            int16,
+            int32,
+            int64
+        }
+
+        private string Category = "EnumListEntries";
+        private IntType CurrentIntType;
+
+        private string[] PreviousEntries;
+        private int entryIndx = 0;
+        private int EntryIndex
+        {
+            get => entryIndx;
+            set
+            {
+                entryIndx = value;
+                if (entryIndx >= PreviousEntries.Length)
+                    entryIndx = 0;
+                else if (entryIndx < 0)
+                    entryIndx = PreviousEntries.Length - 1;
+
+                ParsedType = PreviousEntries[entryIndx];
+            }
+        }
+
+        private void OnEnable() => SetEntries();
+
+        private void SetEntries()
+        {
+            try { PreviousEntries = Hooks.Streaming.LoadAll(Settings.SettingsPath, Category, false).ToArray(); }
+            catch { goto DefaultSetter; }
+
+            if (PreviousEntries != null)
+                return;
+
+        DefaultSetter:
+            PreviousEntries = new string[] { "WWW.EntitiesSystem.Statistics.Stacking" };
+        }
+
+        private void OnGUI()
+        {
+            try
+            {
+                if (!ActivatedWindow)
+                {
+                    if (PreviousEntries == null) SetEntries();
+                    ParsedType = EditorGUILayout.TextField("Type", ParsedType);
+                    flag = GUILayout.Button("Check", GUILayout.Height(40));
+                    CurrentType = ParsedType.Length > 0 ? Hooks.ParseType(ParsedType) : null;
+
+                    if (CurrentType != null && CurrentType.BaseType == typeof(Enum))
+                    {
+                        if (flag)
+                        {
+                            ActivatedWindow = true;
+                            Type underlyingType = Enum.GetUnderlyingType(CurrentType);
+                            CurrentIntType = underlyingType == typeof(int) ? IntType.int32 :
+                                underlyingType == typeof(short) ? IntType.int16 : IntType.int64;
+                            if(PreviousEntries == null || !PreviousEntries.Contains(ParsedType)) Hooks.Streaming.Save(Settings.SettingsPath, Category, $"Entry{PreviousEntries.Length}", ParsedType);
+                            switch (CurrentIntType)
+                            {
+                                default:
+                                    EnumValues = (Enum.GetNames(CurrentType), (int[])Enum.GetValues(CurrentType));
+                                    break;
+                                case IntType.int16:
+                                    EnumValuesShort = (Enum.GetNames(CurrentType), (short[])Enum.GetValues(CurrentType));
+                                    break;
+                                case IntType.int64:
+                                    EnumValuesLong = (Enum.GetNames(CurrentType), (long[])Enum.GetValues(CurrentType));
+                                    break;
+                            }
+                        }
+                    }
+                    else
+                        EditorGUILayout.HelpBox("Parsed type does not exist! Make sure you include the namespace of the type and the type is an Enum.", UnityEditor.MessageType.Warning);
+
+                    if(GUILayout.Button("Previous Entry", GUILayout.Height(20)))
+                    {
+                        EntryIndex--;
+                    }
+                }
+                else
+                {
+                    EditorGUILayout.LabelField($"Currently displaying: {CurrentType.FullName}");
+
+                    EditorGUILayout.BeginVertical();
+                    ScrollPosition = EditorGUILayout.BeginScrollView(ScrollPosition);
+
+                    if (CurrentIntType == IntType.int32)
+                    {
+                        for (short i = 0; i < EnumValues.EnumName.Length; i++)
+                        {
+                            EditorGUILayout.LabelField($"{EnumValues.EnumName[i]} = {EnumValues.EnumVal[i]}");
+                        }
+                    }
+                    else if (CurrentIntType == IntType.int16)
+                    {
+                        for (short i = 0; i < EnumValuesShort.EnumName.Length; i++)
+                        {
+                            EditorGUILayout.LabelField($"{EnumValuesShort.EnumName[i]} = {EnumValuesShort.EnumVal[i]}");
+                        }
+                    }
+                    else
+                    {
+                        for (short i = 0; i < EnumValuesLong.EnumName.Length; i++)
+                        {
+                            EditorGUILayout.LabelField($"{EnumValuesLong.EnumName[i]} = {EnumValuesLong.EnumVal[i]}");
+                        }
+                    }
+                    EditorGUILayout.EndScrollView();
+                    EditorGUILayout.EndVertical();
+
+                    if (GUILayout.Button("Reset"))
+                    {
+                        ActivatedWindow = false;
+                        PreviousEntries = null;
+                    }
+                }
+            }
+            catch
+            {
+                ActivatedWindow = false;
+                PreviousEntries = null;
+            }
+        }
+
+    }
+}
